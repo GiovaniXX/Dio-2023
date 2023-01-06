ddd414cce6d7e18966720e7adb726bfd38723f84
GiovaniXX <giovani_chaves@hotmail.com>
2023-01-06 15:22:09 -0300

    ...

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
-public class App {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primmaryStage) throws Exception {
+             primmaryStage.setTitle("Academia dos Devs");
+             Button botao = new Button("Click");
+             botao.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent arg0) {
+                          System.out.println("Click next");             
+                }
+                
+             });
+
+             StackPane root = new StackPane();
+             root.getChildren().addAll(botao);
+             primmaryStage.setScene(new Scene(root, 300, 250));
+             primmaryStage.show();
     }
 }
